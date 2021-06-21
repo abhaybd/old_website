d482d7b03ff0f30aac680d882bea5ec72e68e3c1
Abhay D <a.deshpande012@gmail.com>
2021-06-21 00:20:34 -0700

    Added opensaber, added pagination bar above

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,32 +4,35 @@ $(document).ready(function() {
     pages.addClass('hidden');
     pages.first().removeClass('hidden');
     
-    const bar = $('#pagination-bar');
+    const bars = $('.pagination-bar');
     if (pages.length > 1) {
         // remove any elements in it right now
-        bar.empty()
+        bars.each((i,bar) => $(bar).empty());
         // For each element add in an appropriate pagination button
         for (let i = 1; i <= pages.length; i++) {
-            let listItem = $('<li class="page-item"></li>');
-            let button = $(`<a class="page-link" href="javascript:void(0)">${i}</a>`);
-            listItem.append(button);
-            bar.append(listItem);
+            bars.each((j, b) => {
+                let bar = $(b);
+                let listItem = $('<li class="page-item"></li>');
+                let button = $(`<a class="page-link" href="javascript:void(0)">${i}</a>`);
+                listItem.append(button);
+                bar.append(listItem);
+            })
         }
         // Mark the first pagination button as active
-        bar.children().first().addClass('active');
+        bars.each((j,bar) => $(bar).children().first().addClass('active'));
         
         // Add the onclick listeners
-        bar.children().click(createOnClick());
+        bars.each((j,bar) => $(bar).children().click(createOnClick()));
     } else {
         // Remove the pagination bar if there is only 1 page
-        bar.remove();
+        bars.remove();
     }
 });
 
 function createOnClick() {
     return function() {
         let index = parseInt($(this).text()) - 1;
-        let currIndex = parseInt($('#pagination-bar .active').first().text()) - 1;
+        let currIndex = parseInt($('.pagination-bar .active').first().first().text()) - 1;
         if (index != currIndex) {
             const pages = $('.carousel-page');
             // hide the old page
@@ -44,13 +47,12 @@ function createOnClick() {
             waitNFrames(2, ()=>newPage.removeClass(newPageClass));
 
             // deactivate all buttons and activate the selected button
-            $('#pagination-bar').children().removeClass('active');
-            $(this).addClass('active');
-
-            // Shamelessly stolen off of SO, scroll to the top of the project grid
-            // $([document.documentElement, document.body]).animate({
-            //     scrollTop: newPage.children().first().offset().top
-            // }, 100);
+            $('.pagination-bar').children().removeClass('active');
+            
+            $('.pagination-bar').each((j,b) => {
+                let bar = $(b);
+                let child = $(bar.children()[index]).addClass('active');
+            });
         }
     }
 }
